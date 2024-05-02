eb3e618a0cea354acc0d72c08ded395b7fcc2cde
phamhoanghuy100200 <166681223+phamhoanghuy100200@users.noreply.github.com>
2024-05-02 17:30:05 +0700

    Update data.js
    add new project

--- src/app/data.js
+++ src/app/data.js
@@ -3,30 +3,36 @@
 export const projectsData = [
   {
     id: 1,
+    name: "Duolingo-clone",
+    description: "Website allows users to learn english",
+    demoLink: "https://duolingo-huypham.vercel.app/",
+  },
+  {
+    id: 2,
     name: "Messenger-clone",
     description: "Website allows users to send messages",
     demoLink: "https://messenger-clone-huypham.vercel.app",
   },
   {
-    id: 2,
+    id: 3,
     name: "Nertflix-clone",
     description: "Website allows users to watch movies",
     demoLink: "https://nertflix-huypham-clone.vercel.app",
   },
   {
-    id: 3,
+    id: 4,
     name: "Spotify-clone",
     description: "Website allows users to listen to musics",
     demoLink: "https://github.com/phamhoanghuy100200/spotify-clone",
   },
   {
-    id: 4,
+    id: 5,
     name: "Painting Shop",
     description: "This is an e-commerce website for selling painting",
     demoLink: "https://github.com/phamhoanghuy20001002?tab=repositories",
   },
   {
-    id: 5,
+    id: 6,
     name: "Booking care",
     description: "Website allows users to schedule medical appointments",
     demoLink: "https://github.com/phamhoanghuy20001002?tab=repositories",
